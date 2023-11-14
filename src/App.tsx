dd3447c76f38b4ccbd43416050471bd402bc0bcc
david <dsuon04@gmail.com>
2023-11-13 22:05:50 -0800

    test env var

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p> Klee</p>
-        <p> ${import.meta.env.VITE_VALUE}</p>
+        <p>env value: {`${import.meta.env.VITE_VALUE}`}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
